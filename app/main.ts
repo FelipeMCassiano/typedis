59ed4b9adcc32abde931e1deaecbe5ee1a87d115
FelipeMCassiano <felipemcassiano@gmail.com>
2025-08-16 12:48:39 -0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,10 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-    // Handle connection
+    connection.write("+PONG\r\n");
+    connection.on("close", () => {
+        connection.end();
+    });
 });
 
 server.listen(6379, "127.0.0.1");
