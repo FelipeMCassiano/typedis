8911caf9a9a2d361b105e859bbccd03aaa58e37b
FelipeMCassiano <felipemcassiano@gmail.com>
2025-08-16 13:35:16 -0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,7 +6,26 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
     connection.on("data", (data) => {
-        connection.write("+PONG\r\n");
+        const commands = data.toString().split("\n");
+        if (commands[0].startsWith("*")) {
+            const numberOfParamters = parseInt(commands[0].match(/\d+/)![0]);
+
+            const command =
+                numberOfParamters === 1
+                    ? commands[numberOfParamters + 1].trim().toLowerCase()
+                    : commands[numberOfParamters].trim().toLowerCase();
+
+            if (command === "echo") {
+                const message =
+                    commands[numberOfParamters + numberOfParamters].trim();
+                const resp = `$${message.length}\r\n${message}\r\n`;
+                connection.write(resp);
+                return;
+            }
+            if (command === "ping") {
+                connection.write("+PONG\r\n");
+            }
+        }
     });
     connection.on("close", () => {
         connection.end();
